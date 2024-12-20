e3feec0d6cde77e7e6ce4a11a91c74caaa85b614
jykim <wddoods015@gmail.com>
2024-12-20 18:33:21 +0900

    [feat] add bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,45 +9,44 @@
 //   );
 // }
 
-
 // src/app/page.tsx
 // import { useEffect } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { AppDispatch, RootState } from '../redux/store';
 // import { fetchNews } from '../redux/NewsSlice';
-import NewsListContainer from '../components/NewsListContainer';
-import HeadLineContainer from '@/components/HeadLineContainer';
-import SearchModal from '@/components/SearchModal';
-import Menu from '@/components/Menu';
-import Header from '@/components/Header';
-import SummaryModal from '@/components/SummaryModal';
-import CategoryContainer from '@/components/CategoryContainer';
+import NewsListContainer from "../components/NewsListContainer";
+import HeadLineContainer from "@/components/HeadLineContainer";
+import SearchModal from "@/components/SearchModal";
+import Menu from "@/components/Menu";
+import Header from "@/components/Header";
+import SummaryModal from "@/components/SummaryModal";
+import CategoryContainer from "@/components/CategoryContainer";
 
 export default function Home() {
   return (
-    <main className="min-w-[390px] mx-auto p-4 h-auto ml-[10%] mr-[10%]" >
-      <Header/>
-      <Menu/>
-      <SummaryModal/>
-      <SearchModal/>
-      <details open className='mt-24'>
-      <summary className='mb-4 font-bold text-xl'>오늘의 헤드라인</summary>
-        <div className="h-auto scrollbar-hidden">
-      <HeadLineContainer />
-      </div>
-      </details>
-      <div className='h-24'>
-      <CategoryContainer />
-      </div>
-      <div className="h-1000 overflow-y-auto">
-      <NewsListContainer />
-      </div>
-      <div className="pt-[80px] w-full h-[180px] flex flex-col items-center gap-4">
-       <span>© 2024. WI All Rights Reserved.</span>
-       <a href='https://github.com/wddoods015/news-summary'>Github</a>
-
-      </div>
-    </main>
-    
+    <div className="bg-gray-300">
+      <main className="bg-white min-w-[390px] mx-auto p-4 h-auto ml-[10%] mr-[10%]">
+        <Header />
+        <Menu />
+        <SummaryModal />
+        <SearchModal />
+        <details open className="mt-24">
+          <summary className="mb-4 font-bold text-xl">오늘의 헤드라인</summary>
+          <div className="h-auto scrollbar-hidden">
+            <HeadLineContainer />
+          </div>
+        </details>
+        <div className="h-24">
+          <CategoryContainer />
+        </div>
+        <div className="h-1000 overflow-y-auto">
+          <NewsListContainer />
+        </div>
+        <div className="pt-[80px] w-full h-[180px] flex flex-col items-center gap-4">
+          <span>© 2024. WI All Rights Reserved.</span>
+          <a href="https://github.com/wddoods015/news-summary">Github</a>
+        </div>
+      </main>
+    </div>
   );
 }
--- src/components/CategoryContainer.tsx
+++ src/components/CategoryContainer.tsx
@@ -1,33 +1,37 @@
 // src/components/CategoryContainer.tsx
 // 카테고리 버튼 영역
 
-'use client';
+"use client";
 
-import React, { useRef, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '@/redux/store';
-import { setSelectedCategory } from '@/redux/slice/CategorySlice';
-import { fetchNewsByDate, fetchNewsByCategory, resetLoadCount } from '@/redux/slice/NewsSlice';
-import CategoryButton from './CategoryButton';
-
+import React, { useRef, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@/redux/store";
+import { setSelectedCategory } from "@/redux/slice/CategorySlice";
+import {
+  fetchNewsByDate,
+  fetchNewsByCategory,
+  resetLoadCount,
+} from "@/redux/slice/NewsSlice";
+import CategoryButton from "./CategoryButton";
 
 const CategoryContainer: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
   const containerRef = useRef<HTMLDivElement>(null);
   const { categories, selectedCategory, isLoading } = useSelector(
-    (state: RootState) => state.categories);
+    (state: RootState) => state.categories
+  );
 
   // '전체' 카테고리 클릭시 fetchNewsByDate 함수 호출
   useEffect(() => {
-    if (selectedCategory === '전체') {
+    if (selectedCategory === "전체") {
       dispatch(fetchNewsByDate());
     }
   }, [dispatch]);
 
-  const scroll = (direction: 'left' | 'right') => {
+  const scroll = (direction: "left" | "right") => {
     if (containerRef.current) {
-      const scrollAmount = direction === 'left' ? -200 : 200;
-      containerRef.current.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+      const scrollAmount = direction === "left" ? -200 : 200;
+      containerRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
     }
   };
 
@@ -40,7 +44,7 @@ const CategoryContainer: React.FC = () => {
     dispatch(setSelectedCategory(category));
     dispatch(resetLoadCount());
     // '전체' 카테고리 선택 시 현재 날짜의 뉴스를 가져옴
-    if (category === '전체') {
+    if (category === "전체") {
       dispatch(fetchNewsByDate());
     } else {
       dispatch(fetchNewsByCategory(category));
@@ -48,39 +52,41 @@ const CategoryContainer: React.FC = () => {
   };
 
   if (isLoading) {
-    return <div className="flex justify-center">카테고리를 불러오는 중입니다...</div>;
+    return (
+      <div className="flex justify-center">카테고리를 불러오는 중입니다...</div>
+    );
   }
 
   return (
     <div className="relative h-full flex items-center max-w-full py-4 z-0">
       <div className="absolute left-0 top-1/2 -translate-y-1/2 z-10">
         <button
-          onClick={() => scroll('left')}
+          onClick={() => scroll("left")}
           className="p-1 rounded-full bg-white shadow-md hover:bg-gray-100"
         >
           ◀
         </button>
       </div>
-      
+
       <div className="flex-1 flex justify-center overflow-hidden">
-      <div
-        ref={containerRef}
-        className="flex gap-2 overflow-hidden scrollbar-hide px-8 scroll-smooth items-center"
-      >
-        {categories.map((category) => (
-          <CategoryButton
-            key={category}
-            category={category}
-            isSelected={selectedCategory === category}
-            onClick={() => handleCategoryClick(category)}
-          />
-        ))}
-      </div>
+        <div
+          ref={containerRef}
+          className="flex gap-2 overflow-hidden scrollbar-hide px-8 scroll-smooth items-center"
+        >
+          {categories.map((category) => (
+            <CategoryButton
+              key={category}
+              category={category}
+              isSelected={selectedCategory === category}
+              onClick={() => handleCategoryClick(category)}
+            />
+          ))}
+        </div>
       </div>
 
       <div className="absolute right-0 top-1/2 -translate-y-1/2 z-10">
         <button
-          onClick={() => scroll('right')}
+          onClick={() => scroll("right")}
           className="p-1 rounded-full bg-white shadow-md hover:bg-gray-100"
         >
           ▶
